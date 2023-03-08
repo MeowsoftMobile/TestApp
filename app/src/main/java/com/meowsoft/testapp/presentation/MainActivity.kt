374b97b483223915c511396d94ea80c467fd2a46
Jakub Kuc <jkuc@future-processing.com>
2023-03-08 15:48:32 +0100

    requesting permissions

--- app/src/main/java/com/meowsoft/testapp/presentation/MainActivity.kt
+++ app/src/main/java/com/meowsoft/testapp/presentation/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.meowsoft.testapp.presentation
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.meowsoft.testapp.presentation.ui.theme.TestAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+    private lateinit var permissionRequester: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionRequester = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.getForecast()
+        }
+
+        permissionRequester
+            .launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
         setContent {
             TestAppTheme {
-                val viewModel = hiltViewModel<MainActivityViewModel>()
                 // A surface container using the 'background' color from the theme
                 Box(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                   
+
                 }
             }
         }
