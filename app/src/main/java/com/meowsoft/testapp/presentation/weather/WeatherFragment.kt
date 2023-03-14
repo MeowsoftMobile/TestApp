3d159d8f7abe03aa54528e34ac01a9977dfd4642
Jakub Kuc <jkuc@future-processing.com>
2023-03-14 16:59:32 +0100

    basic temperature display

--- app/src/main/java/com/meowsoft/testapp/presentation/weather/WeatherFragment.kt
+++ app/src/main/java/com/meowsoft/testapp/presentation/weather/WeatherFragment.kt
@@ -5,16 +5,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.meowsoft.testapp.domain.location.model.ForecastLocation
+import com.meowsoft.testapp.domain.weather.model.Forecast
 import com.meowsoft.testapp.presentation.ui.theme.TestAppTheme
 import com.meowsoft.testapp.presentation.weather.state.WeatherFragmentEvent
 import dagger.hilt.android.AndroidEntryPoint
@@ -55,7 +62,15 @@ class WeatherFragment : Fragment() {
                 TestAppTheme {
                     val state = viewModel.weatherState.collectAsState()
 
-                    WeatherFragmentContent()
+                    val weather = state.value.forecast
+                    val isLoading = state.value.isLoading
+
+                    if (weather != null) {
+                        WeatherFragmentContent(
+                            weather,
+                            isLoading
+                        )
+                    }
                 }
             }
         }
@@ -75,7 +90,33 @@ class WeatherFragment : Fragment() {
     }
 }
 
-@Preview
 @Composable
-fun WeatherFragmentContent() {
+fun WeatherFragmentContent(
+    forecast: Forecast,
+    isLoading: Boolean
+) {
+    val weatherList = forecast
+        .weatherPerDay
+        .entries
+        .map {
+            it.value
+        }.flatten()
+
+    if (isLoading) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(weatherList.size) { index ->
+                Text(
+                    text = "${weatherList[index].temperature} °C"
+                )
+            }
+        }
+    }
 }
